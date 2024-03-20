027d30459784bec81e5901d132d1695ae32d8164
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-03-20 11:39:25 +0300

    Github progect start

---
+++ assets/js/verybl.js
@@ -0,0 +1,11 @@
+
+// ------------ VERAEBLOIS ALL --------------------------------
+
+const searchInput = $('#search-input');
+const userLogo = $('#user-logo');
+
+const projectSum = $('#projects-sum');
+const starsSum = $('#stars-sum');
+
+const userImg = $('#user-img');
+const userIcon = $('#user-icon');
